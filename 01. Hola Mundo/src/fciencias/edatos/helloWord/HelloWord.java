17a9340d86c49ee2c70f43d1db87ec4241347545
EmmanuelCruz <emmanuel_cruzh@ciencias.unam.mx>
2021-09-21 10:31:39 -0500

    Ejemplo de hola mundo con ANT

---
+++ 01. Hola Mundo/src/fciencias/edatos/helloWord/HelloWord.java
@@ -0,0 +1,14 @@
+package fciencias.edatos.HelloWord;
+
+/**
+ * Ejemplo de programa con ANT.
+ * @author Emmanuel Cruz Hernández.
+ * @version 1.0 Septiembre 2021.
+ * @since Estructuras de datos 2022-1.
+ */
+public class HelloWord{
+
+    public static void main(String[] args) {
+        
+    }
+}
