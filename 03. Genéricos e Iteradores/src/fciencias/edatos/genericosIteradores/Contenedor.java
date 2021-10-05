dea3f22943f5637784af9313a4ef1f769976559b
EmmanuelCruz <emmanuel_cruzh@ciencias.unam.mx>
2021-10-05 11:53:09 -0500

    Ejemplo de iteradores

--- 03. Genéricos e Iteradores/src/fciencias/edatos/genericosIteradores/Contenedor.java
+++ 03. Genéricos e Iteradores/src/fciencias/edatos/genericosIteradores/Contenedor.java
@@ -7,58 +7,110 @@ import java.util.Iterator;
  * @version 1.0 2020.
  * @since Estructuras de Datos 2021-2
  */
-public class Contenedor<T, V>{
+public class Contenedor<T>{
 
     /** Atributo genérico. */
-    public T generico1;
+    public T[] arreglo;
 
-    /** Segundo atributo genérico */
-    public V generico2;
+    /** Tope de elementos disponibles. */
+    private int tope;
 
     /**
      * Crea un nuevo contenedor genérico.
-     * @param generico un objeto de tipo T.
-     * @param generico2 un objeto de tipo V.
+     * @param size a la cantidad de elementos del arreglo.
      */
-    public Contenedor(T generico, V generico2){
-        this.generico1 = generico;
-        this.generico2 = generico2;
+    public Contenedor(int size){
+        arreglo = (T[]) new Object[size];
+        tope = 0;
     }
 
     /**
-     * Obtiene el genérico T-
-     * @return el atributo T.
+     * Agrega un elemento al arreglo.
+     * @param elem el elemento a agregar dentro del arreglo.
      */
-    public T getGenerico(){
-        return generico1;
+    public void add(T elem){
+        // Verificar que aun tiene espacio
+        if(tope >= arreglo.length){
+            throw new RuntimeException("Ya no hay espacio disponible");
+        }
+
+        arreglo[tope++] = elem;
     }
 
     /**
-     * Obtiene el genérico V.
-     * @return el genético V.
+     * Regresa un iterador.
+     * @return un iterador del contenedor.
      */
-    public V getGenerico2(){
-        return generico2;
+    public Iterator<T> generaIterador(){
+        return new IteradorContenedor<T>(arreglo, tope);
     }
 
-    @Override
-    public String toString(){
-        return "Genérico 1: " + generico1 + "\nGenérico 2: " + generico2;
+    /**
+     * Iterador de la clase.
+     */
+    public class IteradorContenedor<T> implements Iterator<T>{
+
+        /** Arreglo  */
+        private T[] arreglo;
+
+        /** Tope */
+        private int tope;
+
+        /** Punto de recorrido. */
+        private int apuntador;
+
+        /**
+         * Crea un nuevo iterador.
+         * @param arreglo el arreglo de elementos
+         * @param tope el tope del recorrido.
+         */
+        public IteradorContenedor(T[] arreglo, int tope){
+            this.arreglo = arreglo;
+            this.tope = tope;
+            this.apuntador = 0;
+        }
+
+        @Override
+        public boolean hasNext(){
+            if(apuntador<tope)
+                return true;
+            return false;
+        }
+
+        @Override
+        public T next(){
+            return arreglo[apuntador++];
+        }
     }
 
     public static void main(String[] args) {
-        Contenedor<String, String> contenedorString = new Contenedor<String, String>("Hola", "mundo");
-        Contenedor<Integer, Float> contenedorInteger = new Contenedor<>(1, 4.5f);
-        Contenedor<Boolean, String> contenedorBoolean = new Contenedor<>(true, "Verdadero");
-        Contenedor<Integer, Integer> contenedorIntegerI = new Contenedor<>(5, 1);
+        Contenedor<String> contenedorString = new Contenedor<>(10);
+        contenedorString.add("Hola");
+        contenedorString.add("mundo");
+        contenedorString.add("Una rola");
+        contenedorString.add("Yonaguni");
+        contenedorString.add("Al Zuri");
+        contenedorString.add("El ultimo");
+        contenedorString.add("El ultimo");
+        contenedorString.add("El ultimo");
+        contenedorString.add("El ultimo");
+        contenedorString.add("El ultimo");
 
-        System.out.println(contenedorString);
+        Iterator<String> iterador = contenedorString.generaIterador();
+        
+        while(iterador.hasNext()){
+            String elem = iterador.next();
+            System.out.println(elem);
+        } // O(n)
 
-        System.out.println(contenedorBoolean);
+        System.out.println("\nSegunda iteración");
 
-        System.out.println(contenedorInteger.getGenerico() + contenedorInteger.getGenerico2());
-
-        // Contenedor<Cancion> contenedorCancion = new Cancion<>(new Cancion(), new Cancion());
+        iterador = contenedorString.generaIterador();
+        
+        while(iterador.hasNext()){
+            String elem = iterador.next();
+            System.out.println(elem);
+        }
     }
 
 }
