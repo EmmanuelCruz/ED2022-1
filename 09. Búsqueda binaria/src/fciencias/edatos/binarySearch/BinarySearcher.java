49698840d606eb6f704f4d2ff456a48f9100df45
EmmanuelCruz <emmanuel_cruzh@ciencias.unam.mx>
2021-11-04 12:01:51 -0600

    Implementación de búsqueda binaria

--- 09. Búsqueda binaria/src/fciencias/edatos/binarySearch/BinarySearcher.java
+++ 09. Búsqueda binaria/src/fciencias/edatos/binarySearch/BinarySearcher.java
@@ -1,5 +1,7 @@
 package fciencias.edatos.binarySearch;
 
+import java.util.Arrays;
+
 /**
 * Búsqueda binaria en un arreglo.
 * @author Emmanuel Cruz Hernández.
@@ -13,8 +15,22 @@ public class BinarySearcher{
 	 * @param arr el arreglo donde buscar.
 	 * @param e el elemento a buscar.
 	 */
-	public boolean contains(int[] arr, int e){
-		return false;
+	public static boolean contains(int[] arr, int e){
+		return contains(arr, e, 0, arr.length-1);
+	}
+
+	private static boolean contains(int[] array, int elem, int lo, int hi){
+		if(lo>hi)
+			return false;
+		int mid = lo + ((hi-lo)/2);
+		System.out.println(array[mid]);
+		if(array[mid] == elem)
+			return true;
+
+		if(array[mid] > elem)
+			return contains(array, elem, lo, mid-1);
+		else
+			return contains(array, elem, mid+1, hi);
 	}
 
 	/**
@@ -27,6 +43,10 @@ public class BinarySearcher{
 	}
 
 	public static void main(String[] args) {
-		
+		int[] arr = {1, 5, 7, 9, 12, 34, 57, 80, 134, 567, 894, 900, 930, 989, 1034, 1067, 1134, 2456};
+		int elem = -500;
+		System.out.println("¿El elemento "+elem+
+			" está contenido en "+Arrays.toString(arr)+"? " +
+			contains(arr, elem));
 	}
 }
